638f5e4b26bd891fb2b0dba4fff6b5d1ec967ffb
Aashish <as.aashish345@gmail.com>
2023-06-04 09:38:49 +0545

    first commit

--- new-project/src/Components/AddTransaction.js
+++ new-project/src/Components/AddTransaction.js
@@ -1,70 +1,71 @@
-import React, { useState } from 'react';
-import './addtransaction.css'
+import React, { useState } from "react";
+import "./addtransaction.css";
 
 const AddTransaction = ({ addTransaction }) => {
-  const [text, setText] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate]=useState('');
-  const [category, setCategory]=useState('');
-
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    addTransaction(text, amount,date,category);
+    addTransaction(text, amount, date, category);
 
-    setText('');
-    setAmount('');
-    setDate('');
-    setCategory('');
+    setText("");
+    setAmount("");
+    setDate("");
+    setCategory("");
   };
-const handleCategory=(items)=>{
-  setCategory(items)
-}
+  const handleCategory = (items) => {
+    setCategory(items);
+  };
 
   return (
     <div className="inputs">
       <h4>Add Transaction</h4>
-      <div className='inputfields'>
-      <form onSubmit={handleSubmit}>
-        <div className='category'>
-        <button
-        type="button"
-        className={category === 'income' ? 'active' : ''}
-        onClick={() => handleCategory('income')}
-      >
-        Income
-      </button>
-      <button type="button"
-        className={category === 'expense' ? 'active' : ''}
-        onClick={() => handleCategory('expense')}
-      >
-        Expense
-      </button>
-        </div>
-     
-      
+      <div className="inputfields">
+        <form onSubmit={handleSubmit}>
+          <div className="category">
+            <button
+              type="button"
+              className={category === "income" ? "active" : ""}
+              onClick={() => handleCategory("income")}
+            >
+              Income
+            </button>
+            <button
+              type="button"
+              className={category === "expense" ? "active" : ""}
+              onClick={() => handleCategory("expense")}
+            >
+              Expense
+            </button>
+          </div>
+
           <input
             type="text"
-            placeholder='text'
+            placeholder="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
             required
           />
           <input
-          placeholder='amount'
+            placeholder="amount"
             type="number"
             value={amount}
             onChange={(e) => setAmount(parseFloat(e.target.value))}
             required
           />
-        <input
-        type="date"
-        value={date}
-        onChange={(e)=>setDate(e.target.value)}
-        />
-        <button  className="btn" type="submit">Add</button>
-      </form>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <button className="btn" type="submit">
+            Add
+          </button>
+        </form>
       </div>
     </div>
   );
--- new-project/src/Components/Transaction.js
+++ new-project/src/Components/Transaction.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './Transaction.css'
+import React from "react";
+import "./Transaction.css";
 
 const TransactionList = ({ transactions, deleteTransaction }) => {
   return (
     <div className="transactionlist">
       <p>Transactions</p>
       <ul>
-        {transactions.map(transaction => (
-          <li key={transaction.id}
-          className={transaction.category==='income' ? 'income':"expense"}>
-            <span className='date'>
-            {transaction.date}
-            </span>
-            <span>
-            {transaction.text} 
-
-            </span>
-            <span className='amount'>
+        {transactions.map((transaction) => (
+          <li
+            key={transaction.id}
+            className={transaction.category === "income" ? "income" : "expense"}
+          >
+            <span className="date">{transaction.date}</span>
+            <span>{transaction.text}</span>
+            <span className="amount">
               Rs
-              {transaction.category==='expense'&& '-'}{transaction.amount}
+              {transaction.category === "expense" && "-"}
+              {transaction.amount}
             </span>
             <button onClick={() => deleteTransaction(transaction.id)}>
               Delete
@@ -26,7 +24,6 @@ const TransactionList = ({ transactions, deleteTransaction }) => {
           </li>
         ))}
       </ul>
-    
     </div>
   );
 };
